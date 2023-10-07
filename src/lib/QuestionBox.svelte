0d6770edce5c8b3f09cc2958da5c9b4bc4c2ef9b
indy <indyy058@gmail.com>
2023-10-07 17:47:13 +0700

    add TestPage component

---
+++ src/lib/QuestionBox.svelte
@@ -0,0 +1,7 @@
+<script>
+    export let question;
+</script>
+
+<div class="bg-white text-center flex items-center justify-center w-[550px] h-[200px] rounded-xl mb-[25px]">
+    <h1>{question}</h1>
+</div>
---
+++ src/routes/TestPage.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { onMount } from "svelte";
+    import pb from '../lib/pocketbase';
+    import QuestionBox from "../lib/QuestionBox.svelte";
+
+</script>
+
+<div class="bg-red-500 w-[600px] h-[450px] flex flex-col items-center justify-center rounded-[25px]">
+    <QuestionBox />
+    <div class="grid grid-cols-2 gap-x-[120px] gap-y-8">
+        <button class="bg-white hover:bg-gray-100 rounded-xl p-[5px] w-[150px] h-[75px]">option</button>
+        <button class="bg-white hover:bg-gray-100 rounded-xl p-[5px] w-[150px] h-[75px]">option</button>
+        <button class="bg-white hover:bg-gray-100 rounded-xl p-[5px] w-[150px] h-[75px]">option</button>
+        <button class="bg-white hover:bg-gray-100 rounded-xl p-[5px] w-[150px] h-[75px]">option</button>
+    </div>
+</div>
